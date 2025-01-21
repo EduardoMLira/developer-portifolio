0f87c2ac77f85209fd98218f8e36bdb050179c1b
Eduardo Lira <Marquesliraeduardo@gmail.com>
2025-01-21 17:12:56 -0300

    adição de imagens de hard skills

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/EduardoMLira/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/EduardoMLira/projeto_curriculo/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
